7b78e3fcf9e3fd02376ca0297038092901921ffd
DilinaF <dilina@futuralabs.tech>
2023-05-24 02:25:56 +0530

    Made Changes

--- src/constants/flags.js
+++
@@ -1,9 +0,0 @@
-const imageUrls = [
-  'src/assets/LK.jpg',
-  'src/assets/Lebanon.jpg',
-  'src/assets/US.jpg',
-  'src/assets/India.jpg',
-  'src/assets/Belgium.jpg'
-];
-
-export default imageUrls;
